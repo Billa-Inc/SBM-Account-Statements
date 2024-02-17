fcab21c0b60f0edeba6cbaa2180bcd38db14a356
Pravee Billakota <praveenbillakota@gmail.com>
2024-02-17 01:16:42 -0600

    #SSI-3 Code Base Created.

---
+++ src/main/java/com/service/mn/SbmAccountStatementsApplication.java
@@ -0,0 +1,13 @@
+package com.service.mn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmAccountStatementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmAccountStatementsApplication.class, args);
+	}
+
+}
