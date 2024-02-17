5d5b8c4a86ad95fe188dd3115af5f8ce31fafc74
Pravee Billakota <praveenbillakota@gmail.com>
2024-02-17 01:23:29 -0600

    SSI-3 #comment Main Class Refactored

--- src/main/java/com/service/mn/SBMAccountStatementsMainApplication.java
+++ src/main/java/com/service/mn/SBMAccountStatementsMainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbmAccountStatementsApplication {
+public class SBMAccountStatementsMainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbmAccountStatementsApplication.class, args);
+		SpringApplication.run(SBMAccountStatementsMainApplication.class, args);
 	}
 
 }
